aa1c67d42af26f687803c3fb90f17e1be0137ecf
clownmeat <reenyobby@yahoo.com>
2023-05-12 15:38:08 +0000

    updated and added about me page

--- server.py
+++ server.py
@@ -8,10 +8,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-
+@app.route("/About-Me")
+def aboutMe():
+    return render_template("aboutMe.html")
 
 
 if __name__ == "__main__":
     import gevent.pywsgi
     app_server = gevent.pywsgi.WSGIServer(('localhost', 8080), app)
+    print('jammin on 8080')
     app_server.serve_forever()
